5ad2f61ba84f58b8286104eedf43ef2b67c49519
Fawwaz129 <wazwazzsrhrr@gmail.com>
2023-03-24 09:29:51 +0700

    Create access token when login

--- web/routes/api.php
+++ web/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MotorController;
+use App\Http\Controllers\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,7 @@ use App\Http\Controllers\MotorController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/posts', [MotorController::class, 'index']);
-Route::get('/posts/{id}', [MotorController::class, 'show']);
+Route::get('/posts', [MotorController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [MotorController::class, 'show'])->middleware(['auth:sanctum']);
 Route::get('/posts2/{id}', [MotorController::class, 'show2']);
+Route::post('/login', [AuthenticationController::class, 'login']);
